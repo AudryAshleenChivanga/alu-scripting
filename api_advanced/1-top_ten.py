edec539390b61f270315e92b79679beb9334d810
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-11-09 12:00:55 +0000

    File 2

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+ #!/usr/bin/python3
+"""
+This module contains the top_ten function that queries the Reddit API
+and prints the titles of the top 10 hot posts of a subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts of a subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'python3:top-ten:v1.0 (by /u/yourusername)'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        posts = response.json().get('data', {}).get('children', [])
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Usage: ./1-top_ten.py <subreddit>")
+        sys.exit(1)
+
+    top_ten(sys.argv[1])
