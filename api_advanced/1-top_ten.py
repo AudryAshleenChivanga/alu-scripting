08d69c714ae94de92cfd89d9af598b5d66ee26e4
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-11-11 19:23:22 +0000

    debugging file 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-"""
-Module to query Reddit API for the top ten hot posts of a subreddit
-"""
+"""Module for top_ten function"""
 import requests
-import sys
+
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts for a given subreddit
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Python/3.7'}
+    """Function that queries the Reddit API."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        print(None)
-        return
-
-    try:
-        posts = response.json().get('data').get('children')
-        for post in posts:
+    if response.status_code == 200:
+        data = response.json().get('data').get('children')
+        for post in data:
             print(post.get('data').get('title'))
-    except Exception:
-        print(None)
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
     else:
-        top_ten(sys.argv[1])
+        print(None)
+        
