84216f565a3b0a50941b44fbbe585b3c92db2a64
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-11-11 22:40:25 +0000

    debugged 3

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,54 +1,53 @@
 #!/usr/bin/python3
-""" 3-count.py """
-import json
-import requests
+"""3-count.py"""
+importjson
+importrequests
 
 
-def count_words(subreddit, word_list, after="", count=[]):
-    """ prints a sorted count of given keywords """
+defcount_words(subreddit,word_list,after="",count=[]):
+"""printsasortedcountofgivenkeywords"""
 
-    if after == "":
-        count = [0] * len(word_list)
+ifafter=="":
+count=[0]*len(word_list)
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    request = requests.get(url,
-                           params={'after': after},
-                           allow_redirects=False,
-                           headers={'User-Agent': 'Mozilla/5.0'})
+url="https://www.reddit.com/r/{}/hot.json".format(subreddit)
+request=requests.get(url,
+params={'after':after},
+allow_redirects=False,
+headers={'User-Agent':'Mozilla/5.0'})
 
-    if request.status_code == 200:
-        data = request.json()
+ifrequest.status_code==200:
+data=request.json()
 
-        for topic in (data['data']['children']):
-            for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        count[i] += 1
+fortopicin(data['data']['children']):
+forwordintopic['data']['title'].split():
+foriinrange(len(word_list)):
+ifword_list[i].lower()==word.lower():
+count[i]+=1
 
-        after = data['data']['after']
-        if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
+after=data['data']['after']
+ifafterisNone:
+save=[]
+foriinrange(len(word_list)):
+forjinrange(i+1,len(word_list)):
+ifword_list[i].lower()==word_list[j].lower():
+save.append(j)
+count[i]+=count[j]
 
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (count[j] > count[i] or
-                            (word_list[i] > word_list[j] and
-                             count[j] == count[i])):
-                        aux = count[i]
-                        count[i] = count[j]
-                        count[j] = aux
-                        aux = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = aux
+foriinrange(len(word_list)):
+forjinrange(i,len(word_list)):
+if(count[j]>count[i]or
+(word_list[i]>word_list[j]and
+count[j]==count[i])):
+aux=count[i]
+count[i]=count[j]
+count[j]=aux
+aux=word_list[i]
+word_list[i]=word_list[j]
+word_list[j]=aux
 
-            for i in range(len(word_list)):
-                if (count[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), count[i]))
-        else:
-            count_words(subreddit, word_list, after, count)
-            
+foriinrange(len(word_list)):
+if(count[i]>0)andinotinsave:
+print("{}:{}".format(word_list[i].lower(),count[i]))
+else:
+count_words(subreddit,word_list,after,count)
